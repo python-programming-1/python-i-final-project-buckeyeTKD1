df1a58e380a11570e780338de215efe8eb91b380
buckeyeTKD1 <49502304+buckeyeTKD1@users.noreply.github.com>
2019-06-13 17:11:25 +0800

    Add files via upload

--- final.py
+++ final.py
@@ -1,14 +1,22 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-my_url = 'https://losangeles.craigslist.org/search/sss?query=honda+civic&sort=rel&min_price=1000&max_price=20000'
+kbb_price=6000
+city='losangeles'
+make='nissan'
+model='altima'
+min_price='2000'
+max_price='20000'
+min_year='1990'
+max_year='2019'
+my_url='https://'+city+'.craigslist.org/search/sss?auto_make_model='+make+'%20'+model+'&max_auto_year='+max_year+'&max_price='+max_price+'&min_auto_year='+min_year+'&min_price='+min_price+'&query='+make+'%20'+model+'&sort=rel'
+    
     #open a page 
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 
-    #parser html
+
 page_soup = soup(page_html, "html.parser")
-    #grab each item
 containers = page_soup.findAll('li', {'class':'result-row'})
 
 
@@ -16,27 +24,56 @@ for record in containers:
   name = record.p.a.text
   price = record.findAll('span',{'class':'result-price'})
   pricen= price[0].text
-  print('name:'+name+'      ''price:'+ pricen)
+  
+  if int(pricen.replace('$',''))>kbb_price:
+    print('name:'+name+'      ''price:'+ pricen+':(')
+  else:
+    print('name:'+name+'      ''price:'+ pricen+':)')
 
 print('continute for the rest of pages')
 l=120
-while l<480:
-  my_url='https://losangeles.craigslist.org/search/sss?max_price=20000&min_price=1000&query=honda%20civic''&s='+str(l)+'&sort=rel'
-  uClient = uReq(my_url)
-  page_html = uClient.read()
-  uClient.close()
-
-    #parser html
-  page_soup = soup(page_html, "html.parser")
-    #grab each item
-  containers = page_soup.findAll('li', {'class':'result-row'})
-
-
-  for record in containers:
-    name = record.p.a.text
-    price = record.findAll('span',{'class':'result-price'})
-    pricen= price[0].text
-    print('name:'+name+'      ''price:'+ pricen)
+total=page_soup.findAll('span', {'class':'totalcount'})
+total_number_text=total[0].text
+total_number_value=int(total_number_text)
+while l<total_number_value:
+  if l<1080:
+    from urllib.request import urlopen as uReq
+    from bs4 import BeautifulSoup as soup
+    my_url='https://'+city+'.craigslist.org/search/sss?s='+str(l)+'&auto_make_model='+make+'%20'+model+'&max_auto_year='+max_year+'&max_price='+max_price+'&min_auto_year='+min_year+'&min_price='+min_price+'&query='+make+'%20'+model+'&sort=rel'
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
     l=l+120
+    page_soup = soup(page_html, "html.parser")
+    containers = page_soup.findAll('li', {'class':'result-row'})
+    for record in containers:
+      name = record.p.a.text
+      price = record.findAll('span',{'class':'result-price'})
+      pricen= price[0].text
+  
+      if int(pricen.replace('$',''))>kbb_price:
+        print('name:'+name+'      ''price:'+ pricen+':(')
+      else:
+        print('name:'+name+'      ''price:'+ pricen+':)')
+    
+  elif l>1050:
+    from urllib.request import urlopen as uReq
+    from bs4 import BeautifulSoup as soup
+    my_url='https://'+city+'.craigslist.org/search/sss?s='+str(l)+'&auto_make_model='+make+'%20'+model+'&max_auto_year='+max_year+'&max_price='+max_price+'&min_auto_year='+min_year+'&min_price='+min_price+'&query='+make+'%20'+model+'&sort=rel'
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    l=l+120
+    page_soup = soup(page_html, "html.parser")
+    containers = page_soup.findAll('li', {'class':'result-row'})
+    for record in containers:
+      name = record.p.a.text
+      price = record.findAll('span',{'class':'result-price'})
+      pricen= price[0].text
+  
+      if int(pricen.replace('$',''))>kbb_price:
+        print('name:'+name+'      ''price:'+ pricen+':(')
+      else:
+        print('name:'+name+'      ''price:'+ pricen+':)')
 else:
-  print('end of all listings')
+    print('end of all listings')
